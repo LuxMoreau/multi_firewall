377045821a46250e3501560b0757fbd4952220b1
LuxMoreau <67974405+LuxMoreau@users.noreply.github.com>
2022-08-08 01:37:29 +0200

    Update multi_firewall.sh

--- multi_firewall.sh
+++ multi_firewall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo apt install ufw -y
 sudo apt upgrade -y
+clear
+echo "Enter your static IP address - eg. 123.456.7.89"
+read ip
 function ask_yes_or_no() {
     read -p "$1 ([y]es or [N]o): "
     case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
@@ -13,10 +16,10 @@ then
     echo "Skipped."
     exit 0
 fi
-sudo yes | ufw reset 
+sudo yes | ufw reset
 sudo ufw default deny incoming
 sudo ufw allow ssh
-sudo ufw allow from YOURSTATICIPHERE to any port 3000:3015 proto tcp
+sudo ufw allow from $ip to any port 3000:3015 proto tcp
 sudo ufw allow to any port 30303:30311 proto tcp
 ufw --force enable
 ufw reload
@@ -29,7 +32,27 @@ then
     exit 0
 fi
 sudo ufw delete allow ssh
-sudo ufw allow proto tcp from YOURSTATICIPHERE to any port 22
+sudo ufw allow proto tcp from $ip to any port 22
 sudo ufw reload
 sudo ufw status numbered
 echo "Done, SSH port is now secured (Script by Lux)"
+if [[ "no" == $(ask_yes_or_no "Add a backup IP address? For example your home IP? (https://www.myip.com/)") ]]
+then
+    echo "Skipped."
+    exit 0
+fi
+echo "Enter a backup IP address - eg. 96.69.1.337"
+read ip2
+sudo ufw allow from $ip2 to any port 3000:3015 proto tcp
+sudo ufw reload
+sudo ufw status numbered
+echo "Done, Backup IP rule is added (Script by Lux)"
+if [[ "no" == $(ask_yes_or_no "Also add SSH rule with backup IP?") ]]
+then
+    echo "Skipped."
+    exit 0
+fi
+sudo ufw allow proto tcp from $ip2 to any port 22
+sudo ufw reload
+sudo ufw status numbered
+echo "Done, Backup IP for SSH port is added (Script by Lux)"
