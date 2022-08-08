2a4c019306ee452037f184a358fb8fa83aa1aa57
LuxMoreau <67974405+LuxMoreau@users.noreply.github.com>
2022-08-08 02:18:55 +0200

    Update multi_firewall.sh

--- multi_firewall.sh
+++ multi_firewall.sh
@@ -13,7 +13,7 @@ function ask_yes_or_no() {
 }
 if [[ "no" == $(ask_yes_or_no "Did you add the correct IP? Continuing will reset your current UFW config. Proceed?") ]]
 then
-    echo "Skipped."
+    echo "Skipped. Re-run the script."
     exit 0
 fi
 sudo yes | ufw reset
@@ -24,35 +24,25 @@ sudo ufw allow to any port 30303:30311 proto tcp
 ufw --force enable
 ufw reload
 ufw status numbered
-echo "Done, bot UI secured (Script by Lux)"
-if [[ "no" == $(ask_yes_or_no "Also add port 22 SSH? If you provided a wrong IP, you can lock yourself out of your server. Continue?") || \
+echo "Done, bot web UI is now secured."
+if [[ "yes" == $(ask_yes_or_no "Add a backup IP address? For example your home IP? (https://www.myip.com/)") ]]
+then
+    echo "Enter a backup IP address - eg. 96.69.1.337"
+    read ip2
+    sudo ufw allow from $ip2 to any port 3000:3015 proto tcp
+    sudo ufw reload
+    sudo ufw status numbered
+    echo "Done, Backup IP rule is added (Script by Lux)"
+fi
+if [[ "no" == $(ask_yes_or_no "Secure SSH/port 22? If you provided a wrong static IP and no backup IP, you can lock yourself out of your server. Continue?") || \
       "no" == $(ask_yes_or_no "Are you REALLY sure?") ]]
 then
-    echo "Skipped."
+    echo "Skipped. End of script. Only bot web UI secured. (Script by Lux)"
     exit 0
 fi
 sudo ufw delete allow ssh
 sudo ufw allow proto tcp from $ip to any port 22
-sudo ufw reload
-sudo ufw status numbered
-echo "Done, SSH port is now secured (Script by Lux)"
-if [[ "no" == $(ask_yes_or_no "Add a backup IP address? For example your home IP? (https://www.myip.com/)") ]]
-then
-    echo "Skipped."
-    exit 0
-fi
-echo "Enter a backup IP address - eg. 96.69.1.337"
-read ip2
-sudo ufw allow from $ip2 to any port 3000:3015 proto tcp
-sudo ufw reload
-sudo ufw status numbered
-echo "Done, Backup IP rule is added (Script by Lux)"
-if [[ "no" == $(ask_yes_or_no "Also add SSH rule with backup IP?") ]]
-then
-    echo "Skipped."
-    exit 0
-fi
 sudo ufw allow proto tcp from $ip2 to any port 22
 sudo ufw reload
 sudo ufw status numbered
-echo "Done, Backup IP for SSH port is added (Script by Lux)"
+echo "Done, SSH port is now secured. (Script by Lux)"
